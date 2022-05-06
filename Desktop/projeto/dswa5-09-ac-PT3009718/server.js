24f1411d1d167e111c2af77554cea30d4065d6ca
carolina <carolinafariacoutinho@gmail.com>
2022-05-05 21:29:14 -0300

    commit

--- Desktop/projeto/dswa5-09-ac-PT3009718/server.js
+++ Desktop/projeto/dswa5-09-ac-PT3009718/server.js
@@ -1,5 +1,27 @@
-var http = require('http');
-var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
-});
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb+srv://Carolina:coutinho@cluster0.msaww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'ifsp';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
